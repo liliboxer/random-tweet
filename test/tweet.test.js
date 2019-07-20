14bccee3c002ddf0be5f0521f49ae66d2ebd6735
Lili Boxer <ninaliboxer@gmail.com>
2019-07-20 11:39:43 -0700

    post tweet

--- test/tweet.test.js
+++ test/tweet.test.js
@@ -7,5 +7,25 @@ const app = require('../lib/app');
 const Tweet = require('../lib/models/Tweet');
 
 describe('tweet routes', () => {
-  
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('POST a tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({ handle: 'maxthecat', text: 'i\'m a cat, meow!' })
+      .then(res => {
+        expect(res.body).toEqual({ _id: expect.any(String), __v: 0, handle: 'maxthecat', text: 'i\'m a cat, meow!' });
+      });
+  });
+
 });
